c98094d03a205ddc964785d2a8b0fc6605ed28bf
Ericadeshh <129593214+Ericadeshh@users.noreply.github.com>
2025-07-24 23:48:16 +0300

    ui update

--- billing_sys_frontend/src/components/PlanCard/PlanCard.tsx
+++ billing_sys_frontend/src/components/PlanCard/PlanCard.tsx
@@ -10,10 +10,14 @@ interface PlanCardProps {
 const PlanCard: React.FC<PlanCardProps> = ({ title, price, device }) => {
   return (
     <div className={styles.card}>
-      <h5>{title}</h5>
-      <p>{device}</p>
-      <div className={styles.price}>{price}</div>
-      <button className="btn btn-primary btn-sm mt-2">Buy</button>
+      <div className={styles.details}>
+        <h5>{title}</h5>
+        <p>{device}</p>
+      </div>
+      <div className={styles.pricing}>
+        <div className={styles.price}>{price}</div>
+        <button className={styles.buyBtn}>Buy</button>
+      </div>
     </div>
   );
 };
